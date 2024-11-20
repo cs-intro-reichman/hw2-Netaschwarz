fcd10414c909130653e117660a51c7b7b48cf428
neta schwarz <netaschwarz@Shemer-MacBookPro-2023.local>
2024-11-20 14:34:36 +0200

    number4

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	   double times = Double.parseDouble(args[0]);
+	   double x = 0;
+	   int timeCount = 0;
+	   for (double i = 1; timeCount < times; i = i+2) {
+			if (i % 4 ==3) {
+				x = x - (1/i);
+			}
+			else if (i % 4 == 1) {
+				x = x + (1/i);
+			}		
+		timeCount++;
+	   }
+	double pi = 4 * x;
+	System.out.println("pi according to Java: " +Math.PI);
+	System.out.println("pi, approximated:     "+pi);
+		
 	}
 }
